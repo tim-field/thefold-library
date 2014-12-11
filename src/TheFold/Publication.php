aa6e6031303d4bf5419d863d6ff6a53cb7808a18
Tim Field <tim@thefold.co.nz>
2014-12-11 14:05:16 +1300

    Tweaks from vend

--- src/TheFold/Publication.php
+++ src/TheFold/Publication.php
@@ -2,18 +2,17 @@
 
 namespace TheFold;
 
-abstract class Publication {
+class Publication {
 
     const PREFIX = 'thefold-publication-';
 
-    function publish($name, \Closure $callback)
+    static function publish($name, \Closure $callback)
     {
         do_action(self::PREFIX.$name, $callback()); 
     }
 
-    function subscribe($name, \Closure $callback)
+    static function subscribe($name, \Closure $callback)
     {
         add_action(self::PREFIX.$name, $callback);
     }
-
 }
